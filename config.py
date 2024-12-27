93a1e648f62753dcf82c56d9896ee03d8f38131c
artisano1 <71612985+artisano1@users.noreply.github.com>
2024-12-27 16:55:34 +0100

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "YOUR_TELEGRAM_BOT_TOKEN")
-    HUBSPOT_ACCESS_TOKEN = os.getenv("HUBSPOT_ACCESS_TOKEN", "YOUR_HUBSPOT_ACCESS_TOKEN")
-    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key")
