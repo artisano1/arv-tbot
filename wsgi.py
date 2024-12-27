cddf8080d4822202f120ef00abce308512b76bc0
artisano1 <71612985+artisano1@users.noreply.github.com>
2024-12-27 16:56:33 +0100

    Delete wsgi.py

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# Add your project directory to Python path
-project_dir = '/home/toparvea/arv-tbot'
-if project_dir not in sys.path:
-    sys.path.append(project_dir)
-
-from app import app as application
